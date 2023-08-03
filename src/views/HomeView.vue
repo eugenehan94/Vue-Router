8a1a15a7d379a4f0dee029e424be898913464eda
eugenehan94 <eugene.han94@gmail.com>
2023-08-03 11:44:19 -0400

    Vue router

---
+++ src/views/HomeView.vue
@@ -0,0 +1,34 @@
+<script setup>
+import cars from "../data/cars.json";
+import { RouterLink } from "vue-router";
+</script>
+
+<template>
+  <div>
+    <h1>Home View</h1>
+    <div class="cars">
+      <RouterLink
+        :to="`/cars/${car.id}`"
+        v-for="car in cars"
+        :key="car.id"
+        href=""
+        alt=""
+      >
+        {{ car.name }}
+      </RouterLink>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.cars {
+  display: flex;
+}
+.cars a {
+  text-decoration: none;
+  color: black;
+  width: 100%;
+  box-shadow: 1px 1px 1px black;
+  margin: 10px;
+}
+</style>
